5c5fc15f2925cdaf8f1073b71f2d31fbbd94c811
Baywatch Rambotics <frc@harborps.org>
2024-02-18 12:03:41 -0500

    Working on various subsystems

--- 7790-2024-Code-V2/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ 7790-2024-Code-V2/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -160,14 +160,14 @@ public class SwerveSubsystem extends SubsystemBase
    */
   public Command driveToPose(Pose2d pose)
   {
-// Create the constraints to use while pathfinding
+    // Create the constraints to use while pathfinding
     PathConstraints constraints = new PathConstraints(
         swerveDrive.getMaximumVelocity(), 4.0,
         swerveDrive.getMaximumAngularVelocity(), Units.degreesToRadians(720));
 
        
 
-// Since AutoBuilder is configured, we can use it to build pathfinding commands
+    // Since AutoBuilder is configured, we can use it to build pathfinding commands
     return AutoBuilder.pathfindToPose(
         pose,
         constraints,
